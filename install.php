f16cf67b6017b4d7fa88ec0174b2bc2939b92929
Chakrit Wichian <buhtig@chakrit.net>
2011-09-28 19:09:39 +0700

    Added table creation SQL.

--- install.php
+++ install.php
@@ -1,6 +1,15 @@
 <?
   include 'db.php';
 
-  mysql_select_db('blogme');
+  $query = <<<EOT
+  CREATE TABLE  `blogme`.`posts` (
+    `blog_id` BIGINT NOT NULL AUTO_INCREMENT,
+    `content` TEXT NOT NULL,
+    `view_count` INT NOT NULL,
+    PRIMARY KEY (`blog_id`)
+  ) ENGINE = MYISAM ;
+  EOT;
+  
+  mysql_query($query) or die("Could not create table `posts`.");
   
 ?>
