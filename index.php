a414d359372d2ee53f825089956d6c2994628c85
Chakrit Wichian <buhtig@chakrit.net>
2011-09-28 19:39:20 +0700

    Implemented index.php

--- index.php
+++ index.php
@@ -1,4 +1,29 @@
-<html><body><h1>It works!</h1>
-<p>This is the default web page for this server.</p>
-<p>The web server software is running but no content has been added, yet.</p>
-</body></html>
+<?php
+  include 'db.php';
+  include 'redis.php';
+  
+  // load posts from database
+  $result = mysql_query("SELECT * FROM posts", $mysql);
+  while ($row = mysql_fetch_array($result)) {
+    $model['posts'][$counter++] = array(
+      'blog_id' => $row['blog_id'],
+      'content' => $row['content'],
+      'view_count' => $row['view_count'])
+  }
+  
+?>
+<html>
+  <body>
+    <h1>It works!</h1>
+    <ul>
+    <?php foreach ($post in $model['posts']) { ?>
+      <li>
+        (<?= $post['view_count'] ?> views)
+        <a href="view.php?blog_id=<?= $post['blog_id'] ?>">
+          <?= substr($post['content'], 0, 20) ?>
+        </a>
+      </li>
+    <?php } ?>
+    </ul>
+  </body>
+</html>
