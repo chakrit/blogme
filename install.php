d1ec73fc27d4549a57454b463a17be464c4ce796
Chakrit Wichian <chakrit@hlpth.com>
2011-09-28 21:11:52 +0700

    Fix sample data script.

--- install.php
+++ install.php
@@ -1,38 +1,37 @@
 <?
   include 'db.php';
 
+  header('Content-Type', 'text/plain');
 
-  echo "Creating table...";
-  flush();
+  function create_table() {
+    global $mysql;
 
-  $query = '
-  CREATE TABLE `blogme`.`posts` (
-    `blog_id` BIGINT NOT NULL AUTO_INCREMENT,
-    `content` TEXT NOT NULL,
-    `view_count` INT NOT NULL,
-    PRIMARY KEY (`blog_id`)
-  ) ENGINE = MYISAM;
-  ';
+    $query = '
+      CREATE TABLE `blogme`.`posts` (
+        `blog_id` BIGINT NOT NULL AUTO_INCREMENT,
+        `content` TEXT NOT NULL,
+        `view_count` INT NOT NULL,
+        PRIMARY KEY (`blog_id`)
+      ) ENGINE = MYISAM;';
   
-  mysql_query($query, $mysql) or die("Could not create table `posts`.");
-  
-  
-  echo "Inserting sample data...";
-  flush();
-  
-  function insert_data(content) {
-    $query = "
-    INSERT INTO `blogme`.`posts` (
-      `content`,
-      `view_count`
-    ) VALUES (
-      `$content`, 0
-    );
-    ";
-  
-    mysql_query($query, $mysql) or die("Error inserting sample data.");
+    mysql_query($query, $mysql) or die("Could not create table `posts`.");
   }
   
+  function insert_data($content) {
+    global $mysql;
+
+    $query = "
+      INSERT INTO `blogme`.`posts` (
+        `content`,
+        `view_count`
+      ) VALUES (
+        '$content', 0
+      )";
+
+    mysql_query($query, $mysql) or die("Error inserting sample data: " . mysql_error($mysql)); 
+  }
+  
+  create_table();
   insert_data("the quick brown fox jumps over the lazy dog.");
   insert_data("the lazy brown dog jumps over the quick fox.");
   insert_data("the quick fox jumps over the lazy brown dog.");
