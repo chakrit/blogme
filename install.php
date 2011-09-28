8d8ed779b9a1a6a9f919a4d4c3fb762e62eccf7b
Chakrit Wichian <buhtig@chakrit.net>
2011-09-28 19:11:50 +0700

    bugfix.

--- install.php
+++ install.php
@@ -1,14 +1,14 @@
 <?
   include 'db.php';
 
-  $query = <<<EOT
+  $query = '
   CREATE TABLE  `blogme`.`posts` (
     `blog_id` BIGINT NOT NULL AUTO_INCREMENT,
     `content` TEXT NOT NULL,
     `view_count` INT NOT NULL,
     PRIMARY KEY (`blog_id`)
   ) ENGINE = MYISAM ;
-  EOT;
+  ';
   
   mysql_query($query) or die("Could not create table `posts`.");
   
