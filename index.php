81e4785753fc9d76cc66285ac65d2ba1fe0ad442
Chakrit Wichian <buhtig@chakrit.net>
2011-09-28 23:40:36 +0700

    First attempt at adding redis cache for frontpage.

--- index.php
+++ index.php
@@ -1,16 +1,57 @@
 <?php
-  include 'include/db.php';
+  include 'include/redis.php';
   
   $counter = 0;
   $posts = array();
   
+  function load_from_db() {
+    global $redis;
+
+    include 'include/db.php';
+    
+    $result = mysql_query("SELECT * FROM posts", $mysql) or die("Error while fetching posts list: " . mysql_error($mysql));
+    while ($row = mysql_fetch_array($result)) {
+      $posts[$counter++] = array(
+        'post_id' => $row['post_id'],
+        'content' => $row['content'],
+        'view_count' => $row['view_count']);
+    }
+    
+    if (!$redis->setnx("posts:list:lock"))
+      return; // only one thread should prime the cache
+    
+    // prime the cache transactionally
+    $multi = $redis->multi();
+    foreach ($posts as $post) {
+      $key = "posts:" . $post['post_id'];
+      $multi = $multi
+        ->hMset($key, $post)
+        ->rpush("posts:list", $key);
+    }
+    
+    $multi = $multi
+      ->expire("posts:list", 1800) // list is cached for half hour
+      ->del("posts:list:lock") // unlocks
+      ->exec();
+  }
+  
+  function load_from_redis() {
+    global $redis;
+    
+    $keys = $redis->lrange("posts:list", 0, -1);
+    $multi = $redis->multi();
+    foreach ($keys as $key) {
+      $multi = $multi->hGetAll($key);
+    }
+    
+    $posts = $multi->exec();
+  }
+  
   // load posts from database
-  $result = mysql_query("SELECT * FROM posts", $mysql) or die("Error while fetching posts list: " . mysql_error($mysql));
-  while ($row = mysql_fetch_array($result)) {
-    $posts[$counter++] = array(
-      'post_id' => $row['post_id'],
-      'content' => $row['content'],
-      'view_count' => $row['view_count']);
+  if ($redis->exists("posts:list")) {
+    load_from_redis();
+  } else {
+    load_from_db();
   }
   
 ?>
