45092f2ab9a97cac2a98fe32a3670c09ddf4111c
Chakrit Wichian <chakrit@hlpth.com>
2011-09-28 18:57:37 +0700

    Initial commit.

---
+++ db.php
@@ -0,0 +1,4 @@
+<?
+  $mysql = mysql_connect('127.0.0.1', 'root', 'root');
+  mysql_select_db('blogme', $mysql) or die('Could not select db.');
+?>
---
+++ install.php
@@ -0,0 +1,6 @@
+<?
+  include 'db.php';
+
+  mysql_select_db('blogme');
+  
+?>
