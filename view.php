17bf6ee7e6a42c1a5646baa8f5cae88d67d9ed00
Chakrit Wichian <buhtig@chakrit.net>
2011-09-29 00:57:16 +0700

    Implement redis in view.php

--- view.php
+++ view.php
@@ -1,22 +1,18 @@
 <?php
-  include 'include/db.php';
-  
+  include 'include/redis.php';
+    
   // validate params
   is_numeric($post_id = $_GET['post_id']) or die("post_id must be a number.");
   
-  // update view count
-  $query = "UPDATE posts SET view_count = (view_count + 1) WHERE post_id = $post_id";
-  mysql_query($query, $mysql) or die("Error incrementing view count: " . mysql_error($mysql));
+  $key = "posts:" . $post_id;
   
-  // fetch display data
-  $query = "SELECT * FROM posts WHERE post_id = $post_id";
-  $result = mysql_query($query, $mysql) or die("Error fetching post $post_id: " . mysql_error($mysql));
-  $result = mysql_fetch_array($result);
+  // only loads from the cache, redirect user to the index so the cache gets primed on first hit
+  if (!$redis-exists()) {
+    header('Location', 'http://127.0.0.1/index.php');
+    exit(0);
+  }
   
-  $post = array(
-    'post_id' => $result['post_id'],
-    'content' => htmlentities($result['content']),
-    'view_count' => $result['view_count']);
+  $post = $redis->hGetAll($key);
   
 ?>
 <html>
